bbf3313151704b5a528aaecf055dd55a2c711c46
Ivan Kuo <mrivankuo@gmail.com>
2017-05-18 19:41:19 -0400

    test commit

--- 02-data-types/assignment/decoder.js
+++ 02-data-types/assignment/decoder.js
@@ -5,6 +5,8 @@
 
  // Your code here
 
+const code = [ ];
+
  /**
   * Question 2
   * Add the following characters to the end of the code array:
@@ -12,44 +14,69 @@
   * 'i', 't', 'l', 'a', 'v', 'o', 'x', 'e', 'r',
   * 'o', 'm', 'c'
   */
-
-// Your code here
-
+code.push('x', 'u', 's', 'a', 'e', 'l', 'p', 'e', 'e', 'n', 'i', 't', 'l', 'a', 'v', 'o', 'x', 'e', 'r', 'o', 'm', 'c')
+console.log(code)
 /**
  * Question 3
  * Remove the first and last elements of the array.
  * Hint: You may need to look up a method we haven't talked about.
  */
+console.log('Second part begins below');
+console.log('');
+const codeLength = code.length
+const killEnd = codeLength - 2 //im sure there's a better way than this..but this took me forever
+const codePop = code.splice(1, killEnd)
+console.log(codePop);
 
+//Nonsense below...
+// const codeShift = codePop.splice((codePop.length), 1)
+// console.log(codePop.length);
+// const truncatedCode = code.splice(0, codeLength + 1)
+// console.log(truncatedCode);
+// console.log(codeShift);
+// console.log(code.length);
 // Your code here
 
 /**
  * Question 4
  * Reverse the array.
  */
-
-// Your code here
-
+console.log('question 4 below');
+console.log(codePop.reverse());
 /**
  * Question 5
  * Replace the last element in the array with an 'e'.
  * Hint: Use length
  */
 
-// Your code here
+console.log('question 5 below');
+// i know this is not the intented goal of lesson
+// codePop.pop()
+// codePop.push('e')
+// codePop.splice(codePop.length - 1)
+// codePop.push('e')
+
+//figured it out...
+
+codePop[codePop.length - 1] = 'e'
+console.log(codePop);
+
+
+
 
 /**
  * Question 6
  * Replace the 5th element and 14th element with space characters.
  * Hint: remember indexes start at 0!
  */
-
-// Your code here
-
+console.log('Question 6 below');
+codePop[4] = ''
+codePop[13] = ''
+console.log(codePop);
 /**
  * Question 7
  * Join the array with an empty String in between each character and log the
  * result to the console.
  */
-
- // Your code here
+// lol you guys are dumb
+console.log(codePop.join(' '));
--- 02-data-types/assignment/distance.js
+++ 02-data-types/assignment/distance.js
@@ -1,11 +1,41 @@
-var X1 = 1
-var Y1 = 3
-var X2 = 10
-var Y2 = 15
-var comma = ","
+// this is the original stuff...trying the extra credit..
+// apparently you don't need to list const 4 times, just do const x1, x2, y1, y2
 
-var sumXsD2 = (X1 + X2) / 2
-var sumYsD2 = (Y1 + Y2) / 2
-console.log(sumXsD2)
-console.log(sumYsD2)
-console.log(sumXsD2, comma, sumYsD2)
+
+const x1 = 1
+const x2 = 10
+const y1 = 3
+const y2 = 15
+
+const deltaX = x2 - x1
+const deltaY = y2 - y1
+
+const xSum = Math.pow(deltaX, 2)
+const ySum = Math.pow(deltaY, 2)
+
+const deltaSum = xSum + ySum
+const distance = Math.sqrt(deltaSum)
+
+console.log(distance);
+
+console.log('process argv here YOYOYOYO', process.argv);
+
+//bah would love to review the below!
+//
+// process.argv([0], [1], [2], [3])
+//
+// const x1 = process.argv[0];
+// const x2 = process.argv[1];
+// const y1 = process.argv[2];
+// const y2 = process.argv[3];
+//
+// deltaX = x1 - x2
+// deltaY = y1 - y2
+//
+// Math.pow(deltaX, 2)
+// Math.pow(deltaY, 2)
+//
+// deltaSum = deltaX + deltaY
+// Math.sqrt(deltaSum)
+//
+// console.log(deltaSum);
